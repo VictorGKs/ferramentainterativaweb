0e58673997b6cd4b026f73e896dddd1014e21359
VictorGKs <147069855+VictorGKs@users.noreply.github.com>
2024-09-10 08:50:52 -0300

    Add files via upload

---
+++ ferramentaflashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'História',
+    'Quanto tempo durou a 2 Guerra Mundial?',
+    '6 anos (1939-1945)'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz estrela em Inglês?',
+    'Estrela em ingles é STAR'
+)
